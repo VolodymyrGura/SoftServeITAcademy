4e03daa5e87ac748d372afa2beb175e2b8a51aaa
VolodymyrGura <volodymyr482@gmail.com>
2017-12-23 20:28:29 +0200

    Add files via upload

---
+++ SoftServeITAcademy_Homework_4D/SoftServeITAcademy_Homework_4D/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace SoftServeITAcademy_Homework_4D
+{
+    public class Program
+    {
+     private enum Winds
+        {
+            Tquiet = 0, Llight, Pmoderate, Sstrong
+        }
+
+         private enum Precipitations
+        {
+
+            WithoutPrecipitation = 0, Rain, Snow
+        }
+
+      public static void Main(string[] args)
+        {
+            Weather[] weather = new Weather[12];
+            int i;
+
+            Random randomTemperature = new Random();
+            Random randomPressure = new Random();
+            Random random = new Random();
+            string[] windss = { "Tquiet", "Llight", "Pmoderate", "Sstrong" };
+            string[] precipitations = { "WithoutPrecipitation", "Rain", "Snow" };
+            int countTemperature = 0;
+            int countWithoutPrecipitation = 0, countRain = 0, countSnow = 0;
+            double avgTemperature = 0;
+
+            Console.WriteLine("температура    атм.тиск    сила вiтру      опади");
+            for (i = 0; i < 12; i++)
+            {
+                weather[i] = new Weather();
+                weather[i].Temperature = randomTemperature.Next(-10, 40);
+                weather[i].Pressure = randomPressure.Next(0, 1000);
+                weather[i].Wind = ((Winds)random.Next(0, 4)).ToString();
+                weather[i].Precipitation = ((Precipitations)random.Next(0, 3)).ToString();
+                weather[i].Print(weather[i].Temperature, weather[i].Pressure, weather[i].Wind, weather[i].Precipitation);
+            }
+
+            Console.WriteLine("Видрукувати погоду в тi днi, коли тиск був нижче введеного числа");
+            Console.WriteLine("Enter Pressure: (0-1000) ");
+            double newPressure = Convert.ToDouble(Console.ReadLine());
+
+            for (i = 0; i < 12; i++)
+            {
+                // Видрукувати погоду в ті дні, коли тиск був нижче введеного числа
+                if (weather[i].Pressure < newPressure)
+                {
+                    weather[i].Print(weather[i].Temperature, weather[i].Pressure, weather[i].Wind, weather[i].Precipitation);
+                }
+
+                // Обчислити середню температуру безвітряних днів.
+                if ((string)weather[i].Wind == "Tquiet")
+                {
+                    avgTemperature = avgTemperature + weather[i].Temperature;
+                    countTemperature++;
+                }
+
+                // Видрукувати погоду в ті дні, коли тиск був нижче введеного числа
+                if ((string)weather[i].Precipitation == "WithoutPrecipitation")
+                {
+                    countWithoutPrecipitation++;
+                }
+               else if ((string)weather[i].Precipitation == "Rain")
+                {
+                    countRain++;
+                }
+                else if ((string)weather[i].Precipitation == "Snow")
+                {
+                    countSnow++;
+                }
+            }
+
+            Console.WriteLine("Обчислити середню температуру безвiтряних днiв.");
+            Console.WriteLine("AvrTemperature {0}", avgTemperature / countTemperature);
+            Console.WriteLine("Порахувати скiльки днiв падав дощ, снiг, а скiльки було без опадiв");
+            Console.WriteLine("Rain: {0} Snow: {1} WithoutPrecipitation: {2}", countRain, countSnow, countWithoutPrecipitation);
+
+            Console.WriteLine("Видрукувати прогнози погоди в порядку спадання температур");
+            int temp = 0;
+            for (i = 0; i < 12; i++)
+            {
+                for (int sort = 0; sort < 11; sort++)
+                {
+                    if (weather[sort].Temperature > weather[sort + 1].Temperature)
+                    {
+                        temp = (int)weather[sort + 1].Temperature;
+                        weather[sort + 1].Temperature = weather[sort].Temperature;
+                        weather[sort].Temperature = temp;
+                    }
+                }
+            }
+
+            for (i = 0; i < 12; i++)
+            {
+                weather[i].Print(weather[i].Temperature, weather[i].Pressure, weather[i].Wind, weather[i].Precipitation);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SoftServeITAcademy_Homework_4D/SoftServeITAcademy_Homework_4D/Weather.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace SoftServeITAcademy_Homework_4D
+{
+   public class Weather
+    {
+        private double temperature;
+        private double pressure;
+        private string wind;
+        private string precipitation;
+
+        public double Temperature
+        {
+            get
+            {
+                return this.temperature;
+            }
+
+            set
+            {
+                this.temperature = value;
+            }
+        }
+
+        public double Pressure
+        {
+            get
+            {
+                return this.pressure;
+            }
+
+            set
+            {
+                this.pressure = value;
+            }
+        }
+
+        public string Wind
+        {
+            get
+            {
+                return this.wind;
+            }
+
+            set
+            {
+                this.wind = value;
+            }
+        }
+
+        public string Precipitation
+        {
+            get
+            {
+                return this.precipitation;
+            }
+
+            set
+            {
+                this.precipitation = value;
+            }
+        }
+
+        public Weather()
+        {
+        }
+
+        public Weather(double temperatures, double pressures, string winds, string precipitations)
+        {
+            this.temperature = temperatures;
+            this.pressure = pressures;
+            this.wind = winds;
+            this.pressure = pressures;
+        }
+
+        public override string ToString()
+        {
+            return "Temperature " + this.temperature + "Pressure " + this.pressure + "Wind " + this.wind + "Precipitation" + this.precipitation;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // Check for null values and compare run-time types.
+            if (obj == null || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Weather parametr = (Weather)obj;
+            return (this.wind == parametr.wind) && (this.precipitation == parametr.precipitation);
+        }
+
+        public void Print(double temperature, double pressure, string wind, string precipitation)
+        {
+            Console.WriteLine("{0}*C         \t {1} \t   {2} \t   {3} ", this.temperature, this.pressure, this.wind, this.precipitation);
+        }
+    }
+}
