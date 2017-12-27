834be0ff50b0a2095d50e2f6541a9dffeb32b599
VolodymyrGura <volodymyr482@gmail.com>
2017-12-27 18:39:40 +0200

    Add files via upload

---
+++ SoftServeITAcademy_Homework_5D/SoftServeITAcademy_Homework_5D/NewList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeITAcademy_Homework_5D
+{
+    class NewList
+    {
+
+        public  void Print(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(" {0} ", numbers[i]);
+            }
+        }
+
+        public void Add(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers.Add(numbers[i]);
+            }
+        }
+
+
+    }
+}
---
+++ SoftServeITAcademy_Homework_5D/SoftServeITAcademy_Homework_5D/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftServeITAcademy_Homework_5D
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            NewList list = new NewList();
+
+            List<int> numbers = new List<int>();
+            List<int> numbersLeft = new List<int>();
+            List<int> numbersRight = new List<int>();
+            Random random = new Random();
+            int i, n, indexMedian;
+            Console.Write("Enter N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nNumbers: ");
+            for (i = 0; i < n; i++)
+            {
+                numbers.Add(random.Next(0, 10));
+            }
+
+            list.Print(numbers);
+
+            indexMedian = (numbers.Count % 2 == 0) ? (numbers.Count / 2) : (((numbers.Count - 1) / 2) + 1);
+            Console.Write("indexMedian: {0}   {1}", indexMedian, numbers[indexMedian - 1]);
+            numbersRight.Add(numbers[indexMedian - 1]);
+            numbers.Remove(numbers[indexMedian - 1]);
+            for (i = 0; i < numbers.Count; i++)
+            {
+               if (numbers[i] < numbers[indexMedian - 1])
+                {
+                    numbersLeft.Add(numbers[i]);
+                }
+                else
+                {
+                    numbersRight.Add(numbers[i]);
+                }
+            }
+
+            numbers.Clear();
+
+            for (i = 0; i < numbersLeft.Count; i++)
+            {
+                numbers.Add(numbersLeft[i]);
+            }
+
+            Console.Write(" \nNew Numbers Left: ");
+            list.Print(numbersLeft);
+
+            for (i = 0; i < numbersRight.Count; i++)
+            {
+                numbers.Add(numbersRight[i]);
+            }
+
+            Console.Write(" \nNew Numbers Right: ");
+            list.Print(numbersRight);
+
+            Console.Write(" \nNew Numbers: ");
+            list.Print(numbers);
+
+            Console.ReadKey();
+        }
+    }
+}
