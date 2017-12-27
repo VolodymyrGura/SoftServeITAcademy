fe3ed82f1eea1373ce706e6c6efbbfb113b2bd9f
VolodymyrGura <volodymyr482@gmail.com>
2017-12-27 20:41:00 +0200

    Add files via upload

---
+++ SoftServeITAcademy_Homework_5D/SoftServeITAcademy_Homework_5D1/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace SoftServeITAcademy_Homework_4
+{
+    public class Person : ICloneable
+    {
+        private string name;
+        private int birthYear;
+
+        public string Name { get; set; }
+
+        public int BirthYear { get; set; }
+
+        public static bool operator ==(Person first, Person second)
+        {
+            return first.name == second.name;
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public Person()
+        {
+            //this.Input();
+        }
+
+        public Person(string names, int birthYears)
+        {
+            this.name = names;
+            this.birthYear = birthYears;
+        }
+
+        public int Age()
+        {
+            int age = 0;
+            return age = 2017 - this.birthYear;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Input person name: ");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Input person Birth of Year: ");
+            this.birthYear = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void ChangeName()
+        {
+            this.name = "Very Young ";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("\n" + this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return " Name: " + this.name + " Birthday Year " + this.birthYear;
+        }
+
+        public object Clone()
+        {
+            return new Person { name = this.Name, birthYear = this.BirthYear };
+        }
+    }
+}
---
+++ SoftServeITAcademy_Homework_5D/SoftServeITAcademy_Homework_5D1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SoftServeITAcademy_Homework_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Person[] person = new Person[10];
+            Person person0 = new Person { Name = "Vova", BirthYear = 1997 };
+            for (i = 0; i < person.Length; i++)
+            {
+                person[i] = (Person)person0.Clone();
+            }
+
+            foreach (var data in person)
+            {
+                Console.WriteLine("Name: {0} Age: {1}", data.Name, data.Age());
+            }
+
+            foreach (var data in person)
+            {
+                if (data.Age() < 16)
+                {
+                    data.ChangeName();
+                }
+            }
+
+            foreach (var data in person)
+            {
+                data.Output();
+            }
+
+            Console.WriteLine("The same names is:");
+
+            for (i = 0; i < person.Length; ++i)
+            {
+                for (int j = i + 1; j < person.Length; ++j)
+                {
+                    if (person[i] == person[j])
+                    {
+                        Console.WriteLine("------" + person[i].Name + person[i].BirthYear);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
